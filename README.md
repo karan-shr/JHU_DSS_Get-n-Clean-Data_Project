ad3942e42ceeb62e7a0658149aa1402c6ed1cd4f
Karan Sharma <virsharma99@gmail.com>
2015-08-23 20:36:29 +0200

    2nd Commit
    cause of 1st commit error, not many changes

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # Getting and Cleaning Data Course Project
 
-## About:
+**About**:
 This repository contains code and documentation submitted for evaluation to Coursera (and John Hopkins) Getting and Cleaning Data course.
 
-## Author: Karan Sharma
+**Author**:
+Karan Sharma
+
+
+### Note: Commenting scheme followed in the code:
+- the no of **#** before a comment are inversaly related to that comment's importance. In same fashion like github markdown, # signifies the highest level of odering and ## e.g. signifies the next level of ordering. So in short, # signifies the main section and ## signifies a subsection...so on and so forth... 
+- a gap between two # e.g."# #" signifies commented out code. 
+- the hastag (#) followed by '---' is used to separate the script into different regions.
+
+
+##Data Analysis Procedure:
+
+### Pre-analysis steps:
+1. Download and unzip the data folder (UCI HAR Dataset) associated with the assignment.
+2. A Git repository is setup on github and is then locally cloned for working with the data.
+3. README.md markdown file is created to document the steps undertaken during the data analysis.
+
+### run_analysis.R: 
+is a R script to collect, work with, and clean a data set. The basic description of the program flow is presented as follows:
+
+1. The script first sets the working directory to the required data folder.
+
+2. Merging training and test datasets:
+  1. The *X, y* and *subject* data (text) files from **train** and **test** folders are loaded into and stored as data frames in R.
+  2. Data frames *subject_joint, X_joint* and *y_joint* are created by row binding the test and train data files.
+
+3. Loading, cleaning and extracting the required (mean and SD data) using features.txt:
+  1. Load *features.txt* into R.
+  2. Remove round brackets and commas from feature names to simplify feature names.
+  3. Associate cleaned feature names with the *X_joint* data frame. 
+  4. Using *grep* command to extract only the desired (i.e. mean and std) data from the *X_joint* data frame.
+  
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,51 @@
 #------------------------------------------------------------------------------
-# run_analysis.R script
-# 
+#                             run_analysis.R script
+#
+# Steps undertaken in the script:
+# 1) Merging datasets
+
 #------------------------------------------------------------------------------
+
+
+
+## navigating to the appropriate data folder
+setwd("./UCI HAR Dataset")
+
+
+#------------------------------------------------------------------------------
+## Step-1: Merging training and test datasets
+
+### loading and saving data from train and test subfolders
+subject_train<-read.table("./train/subject_train.txt", stringsAsFactors = FALSE)
+subject_test<-read.table("./test/subject_test.txt", stringsAsFactors = FALSE)
+
+X_train<-read.table("./train/X_train.txt", stringsAsFactors = FALSE)
+X_test<-read.table("./test/X_test.txt", stringsAsFactors = FALSE)
+
+y_train<-read.table("./train/y_train.txt", stringsAsFactors = FALSE)
+y_test<-read.table("./test/y_test.txt", stringsAsFactors = FALSE)
+
+### row binding the training and test datasets
+X_joint<-rbind(X_train, X_test)
+y_joint<-rbind(y_train, y_test)
+subject_joint<-rbind(subject_train, subject_test)
+
+
+#------------------------------------------------------------------------------
+## Step-2: Loading features.txt and extracting mean and SD data. 
+
+### loading features.txt and removing cleaning the variable names
+featureNames<-read.table("features.txt",stringsAsFactors = FALSE)
+#### removing the parentheis from the names
+cleanFeatureNames<-gsub("\\(\\)","" ,featureNames[,2])
+#### removing single parenthesis by adding | between the two arguements
+cleanFeatureNames<-gsub("\\(|\\)","",cleanFeatureNames)
+#### removing commas
+cleanFeatureNames<-gsub(",","",cleanFeatureNames)
+
+### associating the cleanedFeatureNames as column names for X_joint data frame
+colnames(X_joint)<- cleanFeatureNames
+
+### subsetting only columns with mean and std from X_joint 
+desiredColumns<-grep("mean|std",colnames(X_joint),ignore.case=TRUE)
+X_desired<-X_joint[,desiredColumns]
